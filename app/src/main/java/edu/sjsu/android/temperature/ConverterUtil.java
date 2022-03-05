3908a4b8a786113e2f36d4502979dd8815159b78
Prabhleen Bagri <prabhleenbagri@gmail.com>
2022-03-04 16:43:21 -0800

    test

--- app/src/main/java/edu/sjsu/android/temperature/ConverterUtil.java
+++ app/src/main/java/edu/sjsu/android/temperature/ConverterUtil.java
@@ -3,11 +3,15 @@ package edu.sjsu.android.temperature;
 public class ConverterUtil {
 
     // converts to Celsius
-    public static float convertFahrenheitToCelsius(float fahrenheit) {
-        return ((fahrenheit - 32) * 5 / 9);
+    public static float calculate(float borrowed, float interest, int months, float insurance) {
+        if(interest==0){
+            float res = (borrowed/months) + insurance;
+            return res;
+        } else{
+            float denom = (float) (1-Math.pow((1+interest),-1*months));
+            float res = borrowed * (interest/denom) + insurance;
+            return res;
+        }
     }
-    // converts to fahrenheit
-    public static float convertCelsiusToFahrenheit(float celsius) {
-        return ((celsius * 9) / 5) + 32;
-    }
+
 }
--- app/src/main/java/edu/sjsu/android/temperature/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/temperature/MainActivity.java
@@ -1,48 +1,104 @@
 package edu.sjsu.android.temperature;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private EditText text;
+    TextView finalResult;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (EditText) findViewById(R.id.editText);
+        finalResult = (TextView) findViewById(R.id.textView2);
     }
 
-    public void onClick(View view){
-     switch (view.getId()) {
-        case R.id.button:
-            RadioButton celsiusButton =
-                    (RadioButton) findViewById(R.id.radioButton);
-            RadioButton fahrenheitButton =
-                    (RadioButton) findViewById(R.id.radioButton2);
-            if (text.getText().length() == 0) {
-                Toast.makeText(this, "Please enter a valid number",
-                        Toast.LENGTH_LONG).show();
-                return;
-            }
+    public float getText(){
+        float inputValue = Float.parseFloat(text.getText().toString());
+        return inputValue;
+    }
+
+    public float insuranceCalculated() {
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        if (checkBox.isChecked()) {
+            checkBox.setChecked(true);
             float inputValue = Float.parseFloat(text.getText().toString());
-            if (celsiusButton.isChecked()) {
-                text.setText(String
-                        .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
-                celsiusButton.setChecked(false);
-                fahrenheitButton.setChecked(true);
-            } else {
-                text.setText(String
-                        .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
-                fahrenheitButton.setChecked(false);
-                celsiusButton.setChecked(true);
-            }
-            break;
+            return (float) (inputValue * 0.1);
+        } else {
+            return 0;
+        }
+    }
+
+
+    public float getInterest() {
+        SeekBar seek = (SeekBar) findViewById(R.id.seekBar);
+        int seekValue = seek.getProgress();
+        float interest = ((seekValue / 1200) * 100);
+        return interest;
+    }
+
+    public int getMonths() {
+        int months = 0;
+       // switch (view.getId()) {
+       //     case R.id.button:
+                RadioButton fifteenYears =
+                        (RadioButton) findViewById(R.id.radioButton);
+                RadioButton twentyYears =
+                        (RadioButton) findViewById(R.id.radioButton2);
+                RadioButton thirtyYears =
+                        (RadioButton) findViewById(R.id.radioButton3);
+
+
+                // float inputValue = Float.parseFloat(text.getText().toString());
+                if (fifteenYears.isChecked()) {
+                    // text.setText(String
+                    //      .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    months = 15;
+                    twentyYears.setChecked(false);
+                    thirtyYears.setChecked(false);
+                    fifteenYears.setChecked(true);
+                } else if (twentyYears.isChecked()) {
+                    // text.setText(String
+                    //   .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    months = 20;
+                    fifteenYears.setChecked(false);
+                    thirtyYears.setChecked(false);
+                    twentyYears.setChecked(true);
+                } else {
+                    fifteenYears.setChecked(false);
+                    thirtyYears.setChecked(true);
+                    twentyYears.setChecked(false);
+                    months = 30;
+                }
+
+        //}
+        return months;
+
+
+    }
+
+    public void onClick(View view) {
+        MainActivity test = new MainActivity();
+        Bundle temp = new Bundle();
+        test.onCreate(temp);
+        float insurance = test.insuranceCalculated();
+        float interest = test.getInterest();
+        int months = test.getMonths();
+        float textInput = test.getText();
+        float result = ConverterUtil.calculate(textInput, interest, months, insurance);
+
+        String monthlyResult = Float.toString(result);
+        finalResult.setText(monthlyResult);
     }
 }
 
-}
